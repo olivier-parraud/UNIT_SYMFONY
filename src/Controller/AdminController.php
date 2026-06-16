8a79ec3d75eb7eea761777c32e29c654dd838a73
Olivier <olivier.parraud@laplateforme.io>
2026-06-16 16:38:19 +0200

    Controller crée
    problème pour créer un user

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
+    #[IsGranted('ROLE_ADMIN')] // ← C'est cette ligne magique qui sécurise la page !
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class UserController extends AbstractController
+{
+    #[Route('/mon-profil', name: 'app_user_profile')]
+    #[IsGranted('ROLE_USER')] // ← Seuls les utilisateurs connectés peuvent entrer ici
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
