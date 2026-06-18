cd3821b7b418082296a2a8b7f914487ffd78828b
Olivier <olivier.parraud@laplateforme.io>
2026-06-18 14:36:48 +0200

    page de connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // Si l'utilisateur est déjà connecté, on peut le laisser sur l'accueil
+        // ou le rediriger ailleurs (ex: vers son profil)
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_user_profile');
+        }
+
+        // Récupère l'erreur de connexion s'il y en a une
+        $error = $authenticationUtils->getLastAuthenticationError();
+        
+        // Dernier nom d'utilisateur saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
 }
